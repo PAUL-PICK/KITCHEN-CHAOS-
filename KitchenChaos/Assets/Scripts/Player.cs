6819ab697f4e23a4eb1014ce0f485a05225042a8
PAUL-PICK <108989810+PAUL-PICK@users.noreply.github.com>
2023-02-22 15:24:01 +0000

    added  Input Class
    Refactored code for input

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -5,31 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour{
     //This is using the old input system
     [SerializeField] private float moveSpeed = 7.0f;
+    [SerializeField] private GameInput gameInput;
    
     private bool isWalking;
 
     private void Update() {
-         Vector2 inputVector = new Vector2(0,0);
 
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-           
-        }
-
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-
-
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.getMovmentVectorNormalized();
 
         // this forces the objects y vector into the worlds z vector
         Vector3 moveDir = new Vector3(inputVector.x ,0f ,inputVector.y);
